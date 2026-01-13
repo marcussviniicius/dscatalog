f1b920d1c4bff22b77c8df9a636c5e2e9a4a0eff
Marcus Vinicius <marcus992marcus@gmail.com>
2026-01-13 12:11:22 -0300

    H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> findAll(){
+        return categoryRepository.findAll();
+    }
+}
