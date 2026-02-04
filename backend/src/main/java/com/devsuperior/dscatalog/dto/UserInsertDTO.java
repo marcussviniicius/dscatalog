a034773d1a7be98b324117977b8a76f439dd1cd1
Marcus Vinicius <marcus992marcus@gmail.com>
2026-02-04 10:20:03 -0300

    Custom constraint validator

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscatalog.dto;
 
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
 import lombok.Data;
 
 @Data
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
     private String password;
